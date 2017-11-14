19935894eb50484bda7ee6913db94bcb1f490118
raghvendra.choubey <choubey.raghvendra@gmail.com>
2017-11-14 19:26:37 +0530

    first proper commit
    Signed-off-by: raghvendra.choubey <choubey.raghvendra@gmail.com>

--- database_connection/views.py
+++ database_connection/views.py
@@ -8,4 +8,9 @@ def index(request):
 
 def login(request):
     # request.
+    u_name = request.GET.get("u_name")
+    id_password = request.GET.get("id_password")
+
+    print("u_name", u_name)
+    print("password", id_password)
     return render(request, 'login.html')
