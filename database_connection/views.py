5bba7cffbb5165ba32b4b7a34cd4c8de2b6cd129
raghvendra.choubey <choubey.raghvendra@gmail.com>
2017-11-14 16:10:03 +0530

    first proper commit
    Signed-off-by: raghvendra.choubey <choubey.raghvendra@gmail.com>

--- database_connection/views.py
+++ database_connection/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    # request.
+    return render(request, 'login.html')
--- python/urls.py
+++ python/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+
+import database_connection.views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'python.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', database_connection.views.index, name='index'),
+    url(r'^login', database_connection.views.login, name='login'),
 ]
